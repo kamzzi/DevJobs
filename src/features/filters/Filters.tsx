0f04f88cb8faafe9c5c539b7738c888a3c791c28
kamzzi <pluzeq@gmail.com>
2024-04-13 09:37:28 +0200

    Filters
    Work with responsivity change UI, work with general responsivity.

--- src/features/filters/Filters.tsx
+++ src/features/filters/Filters.tsx
@@ -1,26 +1,42 @@
-import { Button } from "../ui/Button/Button";
 import styled from "./Filters.module.css";
 import { FiltersCustomCheckbox } from "./FiltersCustomCheckbox/FiltersCustomCheckbox";
 import { FiltersInput } from "./FiltersInput/FiltersInput";
+import { Button } from "../ui/Button/Button";
+import { useMediaQuery } from "react-responsive";
 
 export const Filters = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width:48rem)",
+  });
   const checked = false;
   return (
     <form className={styled.filters}>
-      <FiltersInput
-        id="title"
-        placeholder="Filter by title, companies"
-        icon="search"
-      />
-      <FiltersInput
-        id="location"
-        placeholder="Filter by location..."
-        icon="location"
-      />
-      <div className={styled.filters__item}>
-        <FiltersCustomCheckbox checked={checked} />
-      </div>
-      <Button modifier="form">Search</Button>
+      <FiltersInput id="title" placeholder="Filter by title..." icon="search" />
+      {!isMobile ? (
+        <>
+          <FiltersInput
+            id="location"
+            placeholder="Filter by location..."
+            icon="location"
+          />
+          <div className={styled.filters__item}>
+            <FiltersCustomCheckbox checked={checked} />
+          </div>
+        </>
+      ) : (
+        <div className={styled.filters__item}>
+          <Button aria-title="Open filters">
+            <img src="./assets/mobile/icon-filter.svg" alt="" />
+          </Button>
+        </div>
+      )}
+      <Button modifier="form">
+        {isMobile ? (
+          <img src="./assets/mobile/icon-search.svg" alt="" />
+        ) : (
+          "Search"
+        )}
+      </Button>
     </form>
   );
 };
