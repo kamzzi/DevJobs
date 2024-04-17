11ef956c0fd9df0003d08c54f0cf5eda6ec72ff3
kamzzi <pluzeq@gmail.com>
2024-04-17 18:26:20 +0200

    Not found
    Styling and working with not found page.

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,25 @@
+import { useRouteError } from "react-router";
+import { Global } from "../../layout/Global/Global";
+import { Error as CustomError } from "../../features/ui/Error/Error";
+import styled from "./NotFound.module.css";
+import { RoutesPaths } from "../../types/shared";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-  return <h1>Not Found!</h1>;
+  const error = useRouteError();
+
+  const message =
+    error instanceof Error ? error.message : "Error : (404) Not found.";
+
+  return (
+    <Global>
+      <main className={styled["not-found"]}>
+        <h1 className={styled.title}>Error Occured</h1>
+        <CustomError error={message} />
+        <Link to={RoutesPaths.HOME} className={styled.link}>
+          Back to home
+        </Link>
+      </main>
+    </Global>
+  );
 };
