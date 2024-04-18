84f717c75a64df6c44cc2518176260b583e25283
kamzzi <pluzeq@gmail.com>
2024-04-18 21:09:18 +0200

    Job
    Styling, work with responsivity and content on useMediaQuery.

--- src/features/jobs/Job/JobDescription/JobApply/JobApply.tsx
+++ src/features/jobs/Job/JobDescription/JobApply/JobApply.tsx
@@ -2,6 +2,7 @@ import { JobSchemaType } from "../../JobSchema";
 import styled from "./JobApply.module.css";
 import { CustomLink } from "../../../../ui/CustomLink/CustomLink";
 import { Wrapper } from "../../../../ui/Wrapper/Wrapper";
+import { useMediaQuery } from "react-responsive";
 
 type JobApplyProps = {
   job: JobSchemaType;
@@ -10,14 +11,20 @@ type JobApplyProps = {
 export const JobApply = ({
   job: { position, apply, company },
 }: JobApplyProps) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width:48rem)",
+  });
+
   return (
     <footer className={styled.footer}>
       <Wrapper>
         <div className={styled.footer__content}>
-          <div className={styled.informations__position}>
-            <h2 className={styled.informations__title}>{position}</h2>
-            <p className={styled.informations__company}>{company}</p>
-          </div>
+          {!isMobile && (
+            <div className={styled.informations__position}>
+              <h2 className={styled.informations__title}>{position}</h2>
+              <p className={styled.informations__company}>{company}</p>
+            </div>
+          )}
           <CustomLink to={apply} target="_blank" modifier="apply">
             Apply Now
           </CustomLink>
