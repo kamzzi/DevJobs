e401dae2de6101fd24b71f16d54fc835dbb9e9d1
kamzzi <pluzeq@gmail.com>
2024-04-22 22:09:30 +0200

    Modal
    Fix a modal, remove a focus trap react, add custom own.
    Fix a tests.

--- src/features/modal/Modal.tsx
+++ src/features/modal/Modal.tsx
@@ -1,41 +1,80 @@
 import { IoMdClose } from "react-icons/io";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import styled from "./Modal.module.css";
 import { createPortal } from "react-dom";
 import { Wrapper } from "../ui/Wrapper/Wrapper";
 import { Button } from "../ui/Button/Button";
 import { useModal } from "./ModalContext/useModal";
-import FocusTrap from "focus-trap-react";
 
 type ModalProps = {
   children: ReactNode;
 };
 
 export const Modal = ({ children }: ModalProps) => {
-  const { close } = useModal();
+  const { close, isOpen } = useModal();
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      const modalElement = modalRef.current;
+      //add any focusable HTML element you want to include to this string
+      const focusableElements = modalElement!.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+      );
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+
+      const handleTabKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Tab") {
+          if (event.shiftKey && document.activeElement === firstElement) {
+            event.preventDefault();
+            lastElement.focus();
+          } else if (
+            !event.shiftKey &&
+            document.activeElement === lastElement
+          ) {
+            event.preventDefault();
+            firstElement.focus();
+          }
+        }
+      };
+
+      const handleEscapeKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          close();
+        }
+      };
+
+      modalElement?.addEventListener("keydown", handleTabKeyPress);
+      modalElement?.addEventListener("keydown", handleEscapeKeyPress);
+
+      return () => {
+        modalElement?.removeEventListener("keydown", handleTabKeyPress);
+        modalElement?.removeEventListener("keydown", handleEscapeKeyPress);
+      };
+    }
+  }, [isOpen, close]);
 
   return createPortal(
-    <FocusTrap>
-      <div className={styled.container} data-testid="modal">
-        <div className={styled.overlay} onClick={close} data-testid="overlay" />
-        <div className={styled.modal}>
-          <Wrapper modifier="modal">
-            <div className={styled.modal__content}>
-              <div className={styled.modal__close}>
-                <Button
-                  modifier="modal-close"
-                  aria-label="Close modal"
-                  onClick={close}
-                >
-                  <IoMdClose className={styled.modal__icon} />
-                </Button>
-              </div>
-              {children}
+    <div className={styled.container} data-testid="modal">
+      <div className={styled.overlay} onClick={close} data-testid="overlay" />
+      <div className={styled.modal} ref={modalRef}>
+        <Wrapper modifier="modal">
+          <div className={styled.modal__content}>
+            <div className={styled.modal__close}>
+              <Button
+                modifier="modal-close"
+                aria-label="Close modal"
+                onClick={close}
+              >
+                <IoMdClose className={styled.modal__icon} />
+              </Button>
             </div>
-          </Wrapper>
-        </div>
+            {children}
+          </div>
+        </Wrapper>
       </div>
-    </FocusTrap>,
+    </div>,
     document.body
   );
 };
