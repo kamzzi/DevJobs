2ec0d07c1c6f6d8ecae48dcfa2317de8da44214d
kamzzi <pluzeq@gmail.com>
2024-04-23 17:19:11 +0200

    Clearing
    Clear a unnecesary packages, add error response test in jobs.

--- src/features/jobs/Jobs.test.tsx
+++ src/features/jobs/Jobs.test.tsx
@@ -5,6 +5,9 @@ import {
 } from "@testing-library/react";
 import { Jobs } from "./Jobs";
 import { MemoryRouter } from "react-router";
+import { server } from "../../__mocks__/server";
+import { HttpResponse, http } from "msw";
+import { URL } from "./api/api";
 
 describe("Jobs testing", () => {
   test("Should initialy show loading spinner", async () => {
@@ -34,6 +37,29 @@ describe("Jobs testing", () => {
       const jobs = await screen.findAllByRole("listitem");
 
       expect(jobs).toHaveLength(1);
+    });
+  });
+
+  test("Should show a error when fetching is invalid", async () => {
+    server.resetHandlers(
+      http.get(`${URL}/jobs`, () => {
+        return new HttpResponse(null, { status: 500 });
+      })
+    );
+
+    render(
+      <MemoryRouter>
+        <Jobs />
+      </MemoryRouter>
+    );
+
+    const loading = screen.getByTestId("spinner");
+
+    expect(loading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(loading).then(async () => {
+      const error = screen.getByText(/500/i);
+      expect(error).toBeInTheDocument();
     });
   });
 });
